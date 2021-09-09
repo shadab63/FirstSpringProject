29239f7f98ab096b312829cc1a9c426ef7b3ed7c
shadab63 <er.shadabahmad363@gmail.com>
2021-09-09 11:59:14 +0530

    removing comments

---
+++ src/main/java/com/example/demo/FirstSpringProject/controller/NAME.java
@@ -0,0 +1,15 @@
+package com.example.demo.FirstSpringProject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+public class NAME {
+    @RestController
+    @RequestMapping("/User")
+    Public clss UserController {
+        @GetMapping ("/creatUser")
+                public String creatUser () { return "shadab ahmad"; }
+
+    }
+}
